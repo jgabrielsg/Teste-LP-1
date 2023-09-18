60f447238c61d77fcc1223fa2dbc7f8a3ab64f9b
JOÃO GABRIEL MACHADO <B51065@fgv.edu.br>
2023-09-18 08:07:16 -0300

    Função novas

--- calculus.py
+++ calculus.py
@@ -1,13 +1,6 @@
 import pandas as pd
 
-datas = input().split(" - ")
-
-data1 = datas[0]
-data2 = datas[1]
-
-data1 = data1.split(" de ")
-data2 = data2.split(" de ")
-
+# Dicionário de meses
 meses = {
     "Janeiro": 1,
     "Fevereiro": 2,
@@ -23,26 +16,55 @@ meses = {
     "Dezembro": 12
 }
 
-dia1 = data1[0]
-mes1 = data1[1]
-numero_mes1 = meses.get(mes1)
-ano1 = data1[2]
+def input_data():
+    """
+    Solicita ao usuário duas datas no formato "dia de mês de ano" separadas por " - ".
+    """
+    try:
+        datas = input("Digite duas datas no formato 'dia de mês de ano' separadas por ' - ': ").split(" - ")
+        return datas
+    except Exception as e:
+        print("Erro!", (e))
+        return None
 
-dia2 = data2[0]
-mes2 = data2[1]
-numero_mes2 = meses.get(mes2)
-ano2 = data2[2]
+def transform_data(datas):
+    """
+    Converte as datas no formato "dia de mês de ano" para objetos datetime.
 
+    Args:
+        datas (list): Uma lista contendo as duas datas no formato "dia de mês de ano".
 
-data_completa1 = str(dia1) + "-" + str(numero_mes1) + "-" + str(ano1)
-data_completa2 = str(dia2) + "-" + str(numero_mes2) + "-" + str(ano2)
+    Returns:
+        tuple: Uma tupla contendo os objetos datetime correspondentes às datas.
+    """
+    data1 = datas[0]
+    data2 = datas[1]
 
-data_completa1 = pd.to_datetime(data_completa1, format='%d-%m-%Y')
-data_completa2 = pd.to_datetime(data_completa2, format="%d-%m-%Y")
+    data1 = data1.split(" de ")
+    data2 = data2.split(" de ")
 
-diferenca_dias = (data_completa2 - data_completa1).days
+    dia1 = data1[0]
+    mes1 = data1[1]
+    numero_mes1 = meses.get(mes1)
+    ano1 = data1[2]
 
+    dia2 = data2[0]
+    mes2 = data2[1]
+    numero_mes2 = meses.get(mes2)
+    ano2 = data2[2]
 
+    data_completa1 = str(dia1) + "-" + str(numero_mes1) + "-" + str(ano1)
+    data_completa2 = str(dia2) + "-" + str(numero_mes2) + "-" + str(ano2)
 
+    data_completa1 = pd.to_datetime(data_completa1, format='%d-%m-%Y')
+    data_completa2 = pd.to_datetime(data_completa2, format="%d-%m-%Y")
 
-print(abs(diferenca_dias))
+    return data_completa1, data_completa2
+
+if __name__ == "__main__":
+    datas = input_data()
+    
+    if datas:
+        data1, data2 = transform_data(datas)
+        diferenca_dias = abs((data2 - data1).days)
+        print(f"A diferença em dias entre as datas é: {diferenca_dias}")
